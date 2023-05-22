aa1b27f8212de78e2ef3feddee6d7d15dbd260f6
謝侑勳 <80185569+XunZhang-anson@users.noreply.github.com>
2023-05-22 13:21:52 +0800

    雙人遊戲簡略板完成

--- lib/pages/countcoin1.dart
+++ lib/pages/countcoin1.dart
@@ -11,6 +11,7 @@ class Coin1Page extends StatefulWidget {
 
 class _Coin1PageState extends State<Coin1Page> {
   int _count = 0, _number = 0;
+  bool _player = true;
 
   @override
   void initState() {
@@ -21,6 +22,7 @@ class _Coin1PageState extends State<Coin1Page> {
   void _startNewGame() {
     setState(() {
       _count = Random().nextInt(16) + 15;
+      _switch();
       /*_guessNumber = null;
       _guessesLeft = 5;
       _hintText = '從一個1~100猜一個數字(有5次機會)';
@@ -55,65 +57,225 @@ class _Coin1PageState extends State<Coin1Page> {
                             children: [
                               InkWell(
                                   borderRadius: BorderRadius.circular(100),
-                                  onTap: () {
-                                    setState(() {
-                                      _count -= 1;
-                                    });
-                                  },
+                                  onTap: _player || _count < 1
+                                      ? null
+                                      : () {
+                                          setState(() {
+                                            _count -= 1;
+                                            _switch();
+                                          });
+                                        },
                                   child: Padding(
                                     padding: EdgeInsets.all(10),
-                                    child: Text('1'),
+                                    child: Text(
+                                      '1',
+                                      style: TextStyle(
+                                        color: _player || _count < 1
+                                            ? Colors.black
+                                            : Colors.white,
+                                      ),
+                                    ),
                                   )),
                               InkWell(
                                   borderRadius: BorderRadius.circular(100),
-                                  onTap: _number < 2
+                                  onTap: _number < 2 || _player || _count < 2
                                       ? null
                                       : () {
                                           setState(() {
                                             _count -= 2;
+                                            _switch();
                                           });
                                         },
                                   child: Padding(
                                     padding: EdgeInsets.all(10),
-                                    child: Text('2'),
+                                    child: Text(
+                                      '2',
+                                      style: TextStyle(
+                                        color: _number < 2 || _player || _count < 2
+                                            ? Colors.black
+                                            : Colors.white,
+                                      ),
+                                    ),
                                   )),
                               InkWell(
                                   borderRadius: BorderRadius.circular(100),
-                                  onTap: _number < 3
+                                  onTap: _number < 3 || _player || _count < 3
                                       ? null
                                       : () {
                                           setState(() {
                                             _count -= 3;
+                                            _switch();
                                           });
                                         },
                                   child: Padding(
                                     padding: EdgeInsets.all(10),
-                                    child: Text('3'),
+                                    child: Text(
+                                      '3',
+                                      style: TextStyle(
+                                        color: _number < 3 || _player || _count < 3
+                                            ? Colors.black
+                                            : Colors.white,
+                                      ),
+                                    ),
                                   )),
                               InkWell(
                                   borderRadius: BorderRadius.circular(100),
-                                  onTap: _number < 4
+                                  onTap: _number < 4 || _player || _count < 4
                                       ? null
                                       : () {
                                           setState(() {
                                             _count -= 4;
+                                            _switch();
                                           });
                                         },
                                   child: Padding(
                                     padding: EdgeInsets.all(10),
-                                    child: Text('4'),
+                                    child: Text(
+                                      '4',
+                                      style: TextStyle(
+                                        color: _number < 4 || _player || _count < 4
+                                            ? Colors.black
+                                            : Colors.white,
+                                      ),
+                                    ),
                                   )),
                             ],
                           )),
-                      Expanded(flex: 5, child: Container(child: Image.asset('assets/images/coin/coin30.png'))),
+                      Expanded(
+                          flex: 5,
+                          child: Container(
+                              child: Image.asset(
+                                  'assets/images/coin/coin$_count.png'))),
                       Expanded(
                           flex: 0,
-                          child: Row(children: [
-                            IconButton(
-                                icon: Icon(Icons.arrow_forward),
-                                onPressed: null),
-                          ])),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              InkWell(
+                                  borderRadius: BorderRadius.circular(100),
+                                  onTap: !_player || _count < 1
+                                      ? null
+                                      : () {
+                                          setState(() {
+                                            _count -= 1;
+                                            _switch();
+                                          });
+                                        },
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Text(
+                                      '1',
+                                      style: TextStyle(
+                                        color: !_player || _count < 1
+                                            ? Colors.black
+                                            : Colors.white,
+                                      ),
+                                    ),
+                                  )),
+                              InkWell(
+                                  borderRadius: BorderRadius.circular(100),
+                                  onTap: _number < 2 || !_player || _count < 2
+                                      ? null
+                                      : () {
+                                          setState(() {
+                                            _count -= 2;
+                                            _switch();
+                                          });
+                                        },
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Text(
+                                      '2',
+                                      style: TextStyle(
+                                        color: _number < 2 || !_player || _count < 2
+                                            ? Colors.black
+                                            : Colors.white,
+                                      ),
+                                    ),
+                                  )),
+                              InkWell(
+                                  borderRadius: BorderRadius.circular(100),
+                                  onTap: _number < 3 || !_player || _count < 3
+                                      ? null
+                                      : () {
+                                          setState(() {
+                                            _count -= 3;
+                                            _switch();
+                                          });
+                                        },
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Text(
+                                      '3',
+                                      style: TextStyle(
+                                        color: _number < 3 ||
+                                                !_player ||
+                                                _count < 3
+                                            ? Colors.black
+                                            : Colors.white,
+                                      ),
+                                    ),
+                                  )),
+                              InkWell(
+                                  borderRadius: BorderRadius.circular(100),
+                                  onTap: _number < 4 || !_player || _count < 4
+                                      ? null
+                                      : () {
+                                          setState(() {
+                                            _count -= 4;
+                                            _switch();
+                                          });
+                                        },
+                                  child: Padding(
+                                    padding: EdgeInsets.all(10),
+                                    child: Text(
+                                      '4',
+                                      style: TextStyle(
+                                        color: _number < 4 ||
+                                                !_player ||
+                                                _count < 4
+                                            ? Colors.black
+                                            : Colors.white,
+                                      ),
+                                    ),
+                                  )),
+                            ],
+                          )),
                       // Expanded(flex: 1, child: Container(color: Colors.red)),
                     ]))));
+  }
+
+  /*void _dice() {
+    setState(() {
+      _number = Random().nextInt(4) + 1;
+    });
+  }*/
+
+  void _switch() {
+    if (_count > 0) {
+      setState(() {
+        _player = !_player;
+        _number = Random().nextInt(4) + 1;
+      });
+    } else {
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('player${!_player ? 1 : 2} win'),
+            content: Text('恭喜! 你贏了'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  _startNewGame();
+                  Navigator.of(context).pop();
+                },
+                child: Text('重新開始'),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 }
